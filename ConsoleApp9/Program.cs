2284df24e49c912e068c5d2bbe04958e91f9a0f6
Pingvu <nhlpool27@gmail.com>
2023-03-17 13:46:23 +0100

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Träning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur gammal är du?");
+            int ålder = int .Parse(Console.ReadLine());
+
+            if (ålder >= 18)
+            {
+                Console.WriteLine("Du är myndig");
+            }
+            else
+            {
+                Console.WriteLine("Du är inte myndig än");          
+            }
+        }       
+            
+
+        
+    }
+}
